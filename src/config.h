99832a41d082a9ff49e402951e205f8537874853
EGPharoh <55055796+EGPharoh@users.noreply.github.com>
2024-03-23 20:12:02 -0400

    Update config.h

--- src/config.h
+++ src/config.h
@@ -10,8 +10,7 @@ __device__ const int ATTEMPTS_PER_EXECUTION = 200000;
 __device__ const int MAX_PATTERNS = 10;
 
 // exact matches at the beginning of the address, letter ? is wildcard
-__device__ static char const *prefixes[] = {
-  'PHAROH', 'PHAROh', 'PHARoH', 'PHARoh', 'PHArOH', 'PHArOh', 'PHAroH', 'PHAroh', 'PHaROH', 'PHaROh', 'PHaRoH', 'PHaRoh', 'PHarOH', 'PHarOh', 'PHaroH', 'PHaroh', 'PhAROH', 'PhAROh', 'PhARoH', 'PhARoh', 'PhArOH', 'PhArOh', 'PhAroH', 'PhAroh', 'PhaROH', 'PhaROh', 'PhaRoH', 'PhaRoh', 'PharOH', 'PharOh', 'PharoH', 'Pharoh', 'pHAROH', 'pHAROh', 'pHARoH', 'pHARoh', 'pHArOH', 'pHArOh', 'pHAroH', 'pHAroh', 'pHaROH', 'pHaROh', 'pHaRoH', 'pHaRoh', 'pHarOH', 'pHarOh', 'pHaroH', 'pHaroh', 'phAROH', 'phAROh', 'phARoH', 'phARoh', 'phArOH', 'phArOh', 'phAroH', 'phAroh', 'phaROH', 'phaROh', 'phaRoH', 'phaRoh', 'pharOH', 'pharOh', 'pharoH', 'pharoh'
+__device__ static char const *prefixes[] = {"PHAROH", "PHAROh", "PHARoH", "PHARoh", "PHArOH", "PHArOh", "PHAroH", "PHAroh", "PHaROH", "PHaROh", "PHaRoH", "PHaRoh", "PHarOH", "PHarOh", "PHaroH", "PHaroh", "PhAROH", "PhAROh", "PhARoH", "PhARoh", "PhArOH", "PhArOh", "PhAroH", "PhAroh", "PhaROH", "PhaROh", "PhaRoH", "PhaRoh", "PharOH", "PharOh", "PharoH", "Pharoh", "pHAROH", "pHAROh", "pHARoH", "pHARoh", "pHArOH", "pHArOh", "pHAroH", "pHAroh", "pHaROH", "pHaROh", "pHaRoH", "pHaRoh", "pHarOH", "pHarOh", "pHaroH", "pHaroh", "phAROH", "phAROh", "phARoH", "phARoh", "phArOH", "phArOh", "phAroH", "phAroh", "phaROH", "phaROh", "phaRoH", "phaRoh", "pharOH", "pharOh", "pharoH", "pharoh"
 };
 
 #endif
